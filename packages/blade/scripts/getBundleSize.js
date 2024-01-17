84664f1b480c598216d1555eae23ec53c94bb64d
Nitin <nitin.kumar@razorpay.com>
2024-01-17 14:03:54 +0530

    chore: fix memory leak

--- packages/blade/scripts/getBundleSize.js
+++ packages/blade/scripts/getBundleSize.js
@@ -5,8 +5,6 @@ const traverse = require('@babel/traverse').default;
 const execa = require('execa');
 
 const main = () => {
-  const sizeLimitConfig = [];
-
   // Get all the components name exported from the bundle and add them to the size-limit configuration
   const fileContent = fs.readFileSync(
     path.resolve(__dirname, '../build/lib/web/production/components/index.js'),
@@ -18,7 +16,17 @@ const main = () => {
     sourceType: 'module',
   });
 
-  const componentNameList = [];
+  const sizes = [];
+  const excludedComponents = [
+    'useTheme',
+    'BladeProvider',
+    'announce',
+    'clearAnnouncer',
+    'destroyAnnouncer',
+    'getTextProps',
+    'screenReaderStyles',
+    'useActionListContext',
+  ];
 
   // Traverse the AST to get the component names
   traverse(ast, {
@@ -26,63 +34,56 @@ const main = () => {
     ExportSpecifier: ({ node }) => {
       const componentName = node.exported.name;
       // We don't want to add Icon components to the size-limit configuration
-      // We don't want to add the same component name twice, so we'll check if it's already added
-      if (!(componentName.includes('Icon') || componentNameList.includes(componentName))) {
-        componentNameList.push(componentName);
-        sizeLimitConfig.push({
-          name: componentName,
-          path: './build/lib/web/production/components/index.js',
-          import: `{ ${componentName} }`,
-          // Set high limit for the component size so that it doesn't fail the size-limit check
-          limit: '200 kb',
-          running: false,
-          gzip: true,
-        });
+      if (!(componentName.includes('Icon') || excludedComponents.includes(componentName))) {
+        console.log('🚀 Analyzing: ', componentName);
+
+        fs.writeFileSync(
+          path.resolve(__dirname, '../.size-limit.json'),
+          JSON.stringify(
+            [
+              {
+                name: componentName,
+                path: './build/lib/web/production/components/index.js',
+                import: `{ ${componentName} }`,
+                // Set high limit for the component size so that it doesn't fail the size-limit check
+                limit: '200 kb',
+                running: false,
+                gzip: true,
+              },
+            ],
+            null,
+            2,
+          ),
+        );
+
+        const { stdout } = execa.commandSync('yarn size-limit --json');
+
+        const jsonLikeString = stdout
+          .split('\n') // remove new line chars => []
+          .map((item) => item.trim()) // remove whitespace
+          .filter((item) => item !== '') // filter empty array items
+          .join('');
+
+        sizes.push(
+          JSON.parse(
+            jsonLikeString.substring(jsonLikeString.indexOf('[') + 1, jsonLikeString.indexOf(']')),
+          ),
+        );
       }
     },
   });
 
-  // All components import
-  sizeLimitConfig.unshift({
-    name: '*',
-    path: './build/lib/web/production/components/index.js',
-    import: '*',
-    limit: '200 kb',
-    running: false,
-    gzip: true,
-  });
-
-  fs.writeFileSync(
-    path.resolve(__dirname, '../.size-limit.json'),
-    JSON.stringify(sizeLimitConfig, null, 2),
+  const story = fs.readFileSync(
+    path.resolve(__dirname, '../docs/utils/bundleSizeReport.stories.mdx'),
+    'utf-8',
   );
 
-  // Run the size-limit command
-  try {
-    const { stdout } = execa.commandSync('yarn size-limit --json');
+  const updatedStory = story.replace(/BUNDLE_SIZE_DATA/g, JSON.stringify(sizes));
 
-    const jsonLikeString = stdout
-      .split('\n') // remove new line chars => []
-      .map((item) => item.trim()) // remove whitespace
-      .filter((item) => item !== '') // filter empty array items
-      .join('');
-
-    const sizes = JSON.parse(
-      jsonLikeString.substring(jsonLikeString.indexOf('['), jsonLikeString.indexOf(']') + 1),
-    );
-
-    const story = fs.readFileSync(
-      path.resolve(__dirname, '../docs/utils/bundleSizeReport.stories.mdx'),
-      'utf-8',
-    );
-    const updatedStory = story.replace(/BUNDLE_SIZE_DATA/g, JSON.stringify(sizes));
-    fs.writeFileSync(
-      path.resolve(__dirname, '../docs/utils/bundleSizeReport.stories.mdx'),
-      updatedStory,
-    );
-  } catch (error) {
-    throw new Error(error);
-  }
+  fs.writeFileSync(
+    path.resolve(__dirname, '../docs/utils/bundleSizeReport.stories.mdx'),
+    updatedStory,
+  );
 };
 
 main();
